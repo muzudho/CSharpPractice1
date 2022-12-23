7fce118f8c66d0a6dcdc576ae09443fbca9373ea
むずでょ <muzudho1@gmail.com>
2022-12-23 22:49:48 +0900

    EchoDirectory

---
+++ EchoDirectory/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection;
+
+// ディレクトリ・パスをコンソールへ標準出力します
+static void EchoDirectory()
+{
+    string currentDirectory = Directory.GetCurrentDirectory();
+    string executeAssemblyDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "null";
+
+    Console.WriteLine($@"Data
+---------------------------
+Current Directory         : {currentDirectory}
+Execute Assembly Directory: {executeAssemblyDirectory}
+
+Please, Push any key.
+");
+
+    Console.ReadKey();
+}
+
+EchoDirectory();
